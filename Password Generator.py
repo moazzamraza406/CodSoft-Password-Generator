c48c5455b09082fa32ab20bb3bf8f2a1971ff44e
moazzamraza406 <96952674+moazzamraza406@users.noreply.github.com>
2023-11-24 18:52:24 +0500

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,21 @@
+import random as rn
+letters = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'
+           'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z']
+numbers = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9']
+symbols = ['!' , '#' , '$' , '%' , '&' , '*' , '(' , ')' , '+' , '?']
+print("Welcome To Password Generator!")
+n_letters = int(input("How many letters you wnat in your password\n"))
+n_numbers = int(input("How many numbers you wnat in your password\n"))
+n_symbols = int(input("How many symbols you wnat in your password\n"))
+password_list = []
+for x in range(1,n_letters+1):
+    password_list += rn.choice(letters)
+for x in range(1,n_numbers+1):
+    password_list += rn.choice(numbers)
+for x in range(1,n_symbols+1):
+    password_list += rn.choice(symbols)
+rn.shuffle(password_list)
+password = ""
+for x in password_list:
+    password += x
+print(password)
